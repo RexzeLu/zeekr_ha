2743f24da3e8e64ff9ba6bc2931b03a94fa60f71
MengHLS <17862954164@163.com>
2026-06-22 21:12:22 +0800

    修改bug

--- custom_components/zeekr_ev/__init__.py
+++ custom_components/zeekr_ev/__init__.py
@@ -185,3 +185,77 @@ async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Reload config entry."""
     await async_unload_entry(hass, entry)
     await async_setup_entry(hass, entry)
+
+
+# SMS login helpers added below (import-time definitions)
+
+
+
+
+async def _async_setup_sms_entry(hass, entry):
+    from homeassistant.helpers.aiohttp_client import async_get_clientsession
+    from .api_sms import ZeekrSmsApiClient
+    from .coordinator_sms import ZeekrSmsCoordinator
+    session = async_get_clientsession(hass)
+    client = ZeekrSmsApiClient(session)
+    client.store_tokens(entry.data)
+    coordinator = ZeekrSmsCoordinator(hass, client=client, entry=entry)
+    await coordinator.async_config_entry_first_refresh()
+    if coordinator.vehicles:
+        _LOGGER.info("Found %d vehicle(s): %s", len(coordinator.vehicles), ", ".join(v.vin for v in coordinator.vehicles))
+    else:
+        _LOGGER.warning("No vehicles found, will retry on next poll")
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await async_setup_services(hass)
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+    return True
+
+
+async def _async_setup_email_entry(hass, entry):
+    username = entry.data.get(CONF_USERNAME)
+    password = entry.data.get(CONF_PASSWORD)
+    country_code = entry.data.get(CONF_COUNTRY_CODE, "")
+    hmac_access_key = entry.data.get(CONF_HMAC_ACCESS_KEY, "")
+    hmac_secret_key = entry.data.get(CONF_HMAC_SECRET_KEY, "")
+    password_public_key = entry.data.get(CONF_PASSWORD_PUBLIC_KEY, "")
+    prod_secret = entry.data.get(CONF_PROD_SECRET, "")
+    vin_key = entry.data.get(CONF_VIN_KEY, "")
+    vin_iv = entry.data.get(CONF_VIN_IV, "'")
+    if not username or not password:
+        _LOGGER.warning("No username or password")
+        return False
+    use_local_api = entry.data.get(CONF_USE_LOCAL_API, False)
+    try:
+        ZeekrClient = await hass.async_add_executor_job(get_zeekr_client_class, use_local_api)
+    except ImportError as ex:
+        _LOGGER.error("Failed to import zeekr_ev_api: %s", ex)
+        raise ConfigEntryNotReady from ex
+    client = hass.data.get(DOMAIN, {}).pop("_temp_client", None)
+    if client is None or not client.logged_in:
+        client = ZeekrClient(
+            username=username, password=password, country_code=country_code,
+            hmac_access_key=hmac_access_key, hmac_secret_key=hmac_secret_key,
+            password_public_key=password_public_key, prod_secret=prod_secret,
+            vin_key=vin_key, vin_iv=vin_iv, logger=_LOGGER)
+        try:
+            stats = ZeekrRequestStats(hass)
+            await stats.async_load()
+            await stats.async_inc_request()
+            await hass.async_add_executor_job(client.login)
+        except Exception as ex:
+            _LOGGER.error("Could not log in to Zeekr API: %s", ex)
+            raise ConfigEntryNotReady from ex
+    coordinator = ZeekrCoordinator(hass, client=client, entry=entry)
+    await coordinator.async_init_stats()
+    await coordinator.async_config_entry_first_refresh()
+    if coordinator.vehicles:
+        _LOGGER.info("Found %d vehicle(s): %s", len(coordinator.vehicles), ", ".join(v.vin for v in coordinator.vehicles))
+    else:
+        _LOGGER.warning("No vehicles found in account")
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await async_setup_services(hass)
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+    return True
+
---
+++ custom_components/zeekr_ev/setup_helpers.py
@@ -0,0 +1,4 @@
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from .api_sms import ZeekrSmsApiClient
+from .coordinator_sms import ZeekrSmsCoordinator
+
